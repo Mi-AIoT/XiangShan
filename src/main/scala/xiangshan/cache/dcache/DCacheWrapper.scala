e72c4a34d47b32566110d70d9f8b5f5b7869385c
Ruomio <1065940593@qq.com>
2026-04-01 14:14:52 +0800

    refactor(MissQueue): Parallel enqueue
    * remove arbiter, handling four request in same time

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -835,13 +835,18 @@ class MissReadyGen(val n: Int)(implicit p: Parameters) extends XSModule {
   }
   io.in.zipWithIndex.map {
     case (r, idx) => {
-      if (idx == 0) {
-        r.ready := mqReadyVec(idx)
-      } else {
-        r.ready := mqReadyVec(idx) && !Cat(io.in.slice(0, idx).map(_.valid)).orR
-      }
+      r.ready := mqReadyVec(idx)
     }
   }
+  // io.in.zipWithIndex.map {
+  //   case (r, idx) => {
+  //     if (idx == 0) {
+  //       r.ready := mqReadyVec(idx)
+  //     } else {
+  //       r.ready := mqReadyVec(idx) && !Cat(io.in.slice(0, idx).map(_.valid)).orR
+  //     }
+  //   }
+  // }
 
 }
 
@@ -1456,8 +1461,8 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
     missReadyGen.io.in(w + 1) <> ldu(w).io.miss_req
   }
 
-  for (w <- 0 until LoadPipelineWidth) { ldu(w).io.miss_resp := missQueue.io.resp }
-  mainPipe.io.miss_resp := missQueue.io.resp
+  mainPipe.io.miss_resp := missQueue.io.resp(0)
+  for (w <- 0 until LoadPipelineWidth) { ldu(w).io.miss_resp := missQueue.io.resp(w + 1) }
 
   if(StorePrefetchL1Enabled) {
     for (w <- 0 until backendParams.StaCnt) {
@@ -1502,7 +1507,9 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
   wb.io.miss_req_conflict_check(4).bits  := missReqArb.io.out.bits.addr
   missQueue.io.wbq_block_miss_req := wb.io.block_miss_req(4)
 
-  missReqArb.io.out <> missQueue.io.req
+  // todo: remove interface
+  missReqArb.io.out <> DontCare
+  // missQueue.io.req <> DontCare
   missReadyGen.io.queryMQ <> missQueue.io.queryMQ
   io.cmoOpReq <> missQueue.io.cmo_req
   io.cmoOpResp <> missQueue.io.cmo_resp
