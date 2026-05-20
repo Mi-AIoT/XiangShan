13fd3abc56dc0a37a7b26de2d0578cca14468f6f
weiding liu <1045251744@qq.com>
2026-05-20 09:36:38 +0800

    perf(sq): 32X4

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -103,8 +103,8 @@ case class XSCoreParameters
   LoadQueueReplaySize: Int = 72,
   LoadUncacheBufferSize: Int = 16,
   LoadQueueNWriteBanks: Int = 8, // NOTE: make sure that LoadQueueRARSize/LoadQueueRAWSize is divided by LoadQueueNWriteBanks
-  StoreQueuePhysicalSize: Int = 48, // preferably a power of 2
-  StoreQueueMultiple: Int = 2, // preferably a power of 2
+  StoreQueuePhysicalSize: Int = 32, // preferably a power of 2
+  StoreQueueMultiple: Int = 4, // preferably a power of 2
   StoreQueueSnapshotInterval: Int = 1, // must a power of 2
   SQUnalignQueueSize: Int = 2,
   StoreQueueNWriteBanks: Int = 8, // NOTE: make sure that StoreQueueSize is divided by StoreQueueNWriteBanks
