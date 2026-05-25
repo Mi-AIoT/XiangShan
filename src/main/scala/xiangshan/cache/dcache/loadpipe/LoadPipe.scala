24ab2c4bdd2290aec11f17bb160cc00346aaff28
ywlcode <yuwl2024@shanghaitech.edu.cn>
2026-05-25 15:16:10 +0800

    feat(loadpipe): improve write conflict detection with bank-specific checks

--- src/main/scala/xiangshan/cache/dcache/loadpipe/LoadPipe.scala
+++ src/main/scala/xiangshan/cache/dcache/loadpipe/LoadPipe.scala
@@ -139,7 +139,8 @@ class LoadPipe(id: Int)(implicit p: Parameters) extends DCacheModule with HasPer
   assert(RegNext(!(s0_valid && (s0_req.cmd =/= MemoryOpConstants.M_XRD && s0_req.cmd =/= MemoryOpConstants.M_PFR && s0_req.cmd =/= MemoryOpConstants.M_PFW))), "LoadPipe only accepts load req / softprefetch read or write!")
   dump_pipeline_reqs("LoadPipe s0", s0_valid, s0_req)
   
-  val raw_wr_conflict_check = io.writehint.valid && (io.writehint.bits & s0_bank_oh).orR
+  val s0_conflict_check_bank_oh = UIntToOH(addr_to_dcache_bank(io.lsu.req.bits.vaddr))
+  val raw_wr_conflict_check = io.writehint.valid && (io.writehint.bits & s0_conflict_check_bank_oh).orR
   wr_conflict_check := raw_wr_conflict_check && !s0_is_prefetch
   XSPerfAccumulate("wr_conflict_no_ready", io.lsu.req.valid && wr_conflict_check)
   XSPerfAccumulate("wr_conflict_no_ready_prefetch_filtered", io.lsu.req.valid && raw_wr_conflict_check && s0_is_prefetch)
